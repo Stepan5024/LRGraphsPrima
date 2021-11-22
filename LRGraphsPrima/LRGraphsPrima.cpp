f54641f26bd6145f5e64c54f69bc118d7fd4c7ec
Степан <bokarevstepan1@yandex.ru>
2021-11-23 00:32:02 +0300

    Код на защиту с отчетом

--- LRGraphsPrima/LRGraphsPrima.cpp
+++ LRGraphsPrima/LRGraphsPrima.cpp
@@ -88,35 +88,6 @@ int main() {
 
     setlocale(LC_ALL, "Russian");
     system("color F0");
-    Node* list = NULL;
-
-    int x = 0;
-    int value = 0;
-
-    set_edge(&list, 1, 2, 5);
-    set_edge(&list, 1, 4, 3);
-    set_edge(&list, 1, 5, 6);
-
-    set_edge(&list, 2, 7, 2);
-    set_edge(&list, 2, 3, 2);
-
-    set_edge(&list, 3, 6, 1);
-
-    set_edge(&list, 4, 7, 2);
-
-    set_edge(&list, 5, 8, 9);
-    set_edge(&list, 5, 10, 7);
-
-    set_edge(&list, 7, 6, 3);
-
-    set_edge(&list, 8, 3, 5);
-    set_edge(&list, 8, 9, 7);
-
-    set_edge(&list, 9, 6, 8);
-
-    set_edge(&list, 10, 1, 8);
-    set_edge(&list, 10, 8, 1);
-
 
     int graph[V][V] = { { 0, 5, 0, 3, 6, 0, 0, 0, 0, 8 },
                         { 5, 0, 2, 0, 0, 0, 2, 0, 0, 0 },
@@ -129,85 +100,11 @@ int main() {
                         { 0, 0, 0, 0, 0, 8, 0, 7, 0, 0 },
                         { 8, 0, 0, 0, 7, 0, 0, 1, 0, 0 }, };
 
-    // Print the solution
+    cout << "Остовное дерево построенное по алгоритму Прима\n";
+    // построение по Приме
     primMST(graph);
-    primMST(list);
-    /*for (int i = 1; i < vertices - 1; i++) {
-        
-            
-            start = i; //начало пути - нумерация с 1
-            for(int j = start; j < vertices; j++ ){
-                
-                if (find(list, start, j)) //вес пути из startVerticle и currientVerticle или 0, если пути нет
-                {
-                    initData();
-                    finish = j; //конец пути - нумерация с 1
-                    tempRoad[0] = start; //первую точку внесли в маршрут
-                    includedVert[start] = true; //и пометили как включённую
-                    found = false; //путь пока не найден
-                    cout << "\nВершина " << start << endl;
-                    if (step(list, start, finish, 1)) { //ищем кратчайший путь 
-
-                        cout << "Вес: " << start << " ~> " << finish << " равен " << setw(6) << left << weightLen << "\t";
-                        cout << "Путь: ";
-                        for (int i = 0; i < tempWayLen; i++) cout << " ~ " << shortestWay[i];
-                    }
-                    else cout << "Путь не найден! " << j;
-                    cout << endl;
-                }
-                else {} //переход к следующей вершине
-             }
-            
-    } 
-    initData();
-    start = 1; //начало пути - нумерация с 1
-    finish = 10; //конец пути - нумерация с 1
-    tempRoad[0] = start; //первую точку внесли в маршрут
-    includedVert[start] = true; //и пометили как включённую
-    found = false; //путь пока не найден
-    cout << "\nВершина " << start << endl;
-    if (step(list, start, finish, 1)) { //ищем кратчайший путь 
-
-        cout << "Вес: " << start << " ~> " << finish << " равен " << setw(6) << left << weightLen << "\t";
-        cout << "Путь: ";
-        for (int i = 0; i < tempWayLen; i++) cout << " ~ " << shortestWay[i];
-    }
-    else cout << "Путь не найден!";
-    cout << endl;
-    */
 
     return 0;
-}
-
-void print(Node* list) { // вывести на экран
-    int index = 0;
-    for (Node* p = list; p != NULL; p = p->next) {
-        printf("[%d] = %d, %d, %d\n", index, p->s, p->c, p->v);
-        index++;
-    }
-
-    printf("\n");
-}
-void set_edge(Node** plist, int s, int c, int v) { // положить элемент в начало
-    Node* p = (struct Node*)malloc(sizeof(Node));
-    p->s = s;
-    p->c = c;
-    p->v = v;
-    p->next = *plist;
-    *plist = p;
-    paths++;
-    
-}
-void initData() {
-    //Инициализация данных:
-    for (int i = 0; i < vertices; i++) {
-        shortestWay[i] = 0;
-        tempRoad[i] = 0;
-        includedVert[i] = false;
-    }
-    weightLen = 0;
-    cur_len = 0;
-    tempWayLen = 0;
 }
 
 //Функция для поиска вершины с
@@ -232,61 +129,7 @@ void printMST(int parent[], int graph[V][V])
 {
     cout << "Вершины \tВес\n";
     for (int i = 1; i < V; i++)
-        cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << " \n";
-}
-void printMST(int parent[], Node * list)
-{
-    cout << "Вершины \tВес\n";
-    for (int i = 1; i < V; i++)
-        cout << parent[i] << " - " << i << " \t" << find(list, i, parent[i]) << " \n";
-}
-
-void primMST(Node* list) {
-    // Массив для хранения построенного остова
-    int parent[V];
-
-    //Ключевые значения, используемые для выбора минимального веса ребра
-    int key[V];
-
-    // Для прохождения набора вершин, включенных в остов
-    bool mstSet[V];
-
-    // Инициализируйте все ключи как БЕСКОНЕЧНЫЕ
-    for (int i = 0; i < V; i++) {
-        key[i] = INT_MAX;
-        mstSet[i] = false;
-    }
-    // Всегда включайте первую 1-ю вершину в MST.
-    // Сделайте ключ 0, чтобы эта вершина была выбрана в качестве первой вершины.
-    key[0] = 0;
-    parent[0] = -1; // Первый узел всегда является корнем  остова
-
-    // остов будет иметь V вершин
-    for (int count = 0; count < V - 1; count++)
-    {
-        //Выберите минимальную ключевую вершину из
-        //набора вершин, еще не включенных в остов
-        int u = minKey(key, mstSet);
-
-        // Добавьте выбранную вершину в набор остова
-        mstSet[u] = true;
-
-        // Обновить значение ключа и родительский индекс
-        // смежные вершины выбранной вершины.
-        // Рассматривайте только те вершины, которые не являются
-        // все еще включенный в MST
-        for (int v = 0; v < V; v++)
-
-            //граф[u][v] не равен нулю только для смежных вершин m
-            //должно быть установлено значение [v] равно false для вершин, еще не включенных в MST
-            //Обновляйте ключ только в том случае, если график[u][v] меньше, чем ключ[v]
-            
-            if (find(list, u, v) && mstSet[v] == false && find(list, u, v) < key[v])
-                parent[v] = u, key[v] = find(list, u, v);
-    }
-
-    // распечатать построенный MST
-    printMST(parent, list);
+        cout << parent[i] << " - " << i << " \t\t" << graph[i][parent[i]] << " \n";
 }
 
 //Функция для построения и печати MST для
